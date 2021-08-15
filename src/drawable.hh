ee4a46fbb2d2a31922bb9755874c9973e7ec1a84
Patrick Gordon <pgpatrickgordon@gmail.com>
2021-08-15 22:26:34 +0100

    enable srgb

--- src/drawable.hh
+++ src/drawable.hh
@@ -30,6 +30,7 @@ struct drawable {
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         glDisable(GL_SCISSOR_TEST);
+        glEnable(GL_FRAMEBUFFER_SRGB);
         custom_params();
         vao.draw();
         vbo.draw();
