221f7ef375f0cba857e429dff906906cd0aaa6cf
Ava Gordon <avagordon01@gmail.com>
2024-06-27 22:43:49 +0100

    swap wget to lftp for much faster downloads

--- data/lola/download.sh
+++ data/lola/download.sh
@@ -3,11 +3,13 @@
 set -o errexit
 
 #download all text and metadata
-wget --recursive --level=inf --no-parent --continue -e robots=off --ignore-case \
-    --accept htm,html,cat,fmt,txt,lbl,xml \
-    https://pds-geosciences.wustl.edu/lro/lro-l-lola-3-rdr-v1/lrolol_1xxx/
-
-exit
+#lftp is much faster than wget because it is parallel and there are many small files
+#takes about half an hour, downloads abour 4GB
+# lftp -c mirror \
+#     --continue --only-missing --parallel=64 --use-cache \
+#     -I *.htm -I *.html -I *.cat -I *.fmt -I *.txt -I *.lbl -I *.xml \
+#     https://pds-geosciences.wustl.edu/lro/lro-l-lola-3-rdr-v1/lrolol_1xxx/
+# exit
 
 #see lro_mission.txt for explanation of different mission phases
 #commissioning, nominal, science mission, extended science
@@ -18,12 +20,7 @@ exit
 #lro_es_01 - lro_es_136
 
 #download binary data
-wget --recursive --level=inf --no-parent --continue -e robots=off \
-    https://pds-geosciences.wustl.edu/lro/lro-l-lola-3-rdr-v1/lrolol_1xxx/data/lola_rdr/lro_es_110/
-    #https://pds-geosciences.wustl.edu/lro/lro-l-lola-3-rdr-v1/lrolol_1xxx/data/lola_rdr/lro_co/ \
-    #https://pds-geosciences.wustl.edu/lro/lro-l-lola-3-rdr-v1/lrolol_1xxx/data/lola_rdr/lro_no_01/ \
-    #https://pds-geosciences.wustl.edu/lro/lro-l-lola-3-rdr-v1/lrolol_1xxx/data/lola_rdr/lro_no_13/ \
-    #https://pds-geosciences.wustl.edu/lro/lro-l-lola-3-rdr-v1/lrolol_1xxx/data/lola_rdr/lro_sm_01/ \
-    #https://pds-geosciences.wustl.edu/lro/lro-l-lola-3-rdr-v1/lrolol_1xxx/data/lola_rdr/lro_sm_26/ \
-    #https://pds-geosciences.wustl.edu/lro/lro-l-lola-3-rdr-v1/lrolol_1xxx/data/lola_rdr/lro_es_01/ \
-    #https://pds-geosciences.wustl.edu/lro/lro-l-lola-3-rdr-v1/lrolol_1xxx/data/lola_rdr/lro_es_118/
+lftp -c mirror \
+    --continue --only-missing --parallel=64 --use-cache \
+    -I 'data/lola_rdr/lro_es_110/*.dat' \
+    https://pds-geosciences.wustl.edu/lro/lro-l-lola-3-rdr-v1/lrolol_1xxx/
